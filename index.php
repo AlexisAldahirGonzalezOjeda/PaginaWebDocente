d68b46dfed894b745abb3b9024adc07c00f74053
AlexisAldahirGonzalezOjeda <alexisgonzalez120021@gmail.com>
2022-07-27 12:26:12 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body> 
+        <?php
+        require_once 'Controlador/controlador.php';
+        require_once 'Modelo/Conexion.php';
+        $controlador = new Controlador(); 
+        if (filter_input(INPUT_GET, "accion")) {
+            if (filter_input(INPUT_GET, "accion") == "Inicio") {
+                $controlador->verPagina('Vista/html/principal.php'); 
+            }
+            if (filter_input(INPUT_GET, "accion") == "Formulario") {
+                $controlador->verPagina('Vista/html/formulario.php');
+            }
+            if (filter_input(INPUT_GET, "accion") == "Sesion") {
+                $controlador->verPagina('Vista/html/iniciarSesion.php');
+            }
+            if (filter_input(INPUT_GET, "accion") == "Registro") {
+                $controlador->verPagina('Vista/html/registro.php');
+            }
+        } else {
+            $controlador->verPagina('Vista/html/principal.php');
+        }
+        ?>
+    </body>
+</html>
