bd2e32b89d158a83d92a8c1bfd60981f835b7682
AlexisAldahirGonzalezOjeda <alexisgonzalez120021@gmail.com>
2022-07-31 08:42:31 -0500

    Update principal.php

--- Vista/html/principal.php
+++ Vista/html/principal.php
@@ -3,6 +3,7 @@
 <html>
     <head>
         <title>Página Web del Profesor</title>
+        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
         <link href="Vista\imagenes\icono.ico" rel="icon" />
         <link rel="stylesheet" type="text/css" href="Vista/css/estilos.css" media="all and (min-width: 321px)"> 
         <link rel="stylesheet" type="text/css" href="Vista/css/estilosCelular.css" media="all and (min-width: 0px) and (max-width: 320px)">
