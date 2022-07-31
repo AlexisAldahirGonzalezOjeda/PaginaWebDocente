fcbcb0498240d098f4e032a18ffeeb13850add84
AlexisAldahirGonzalezOjeda <alexisgonzalez120021@gmail.com>
2022-07-30 21:52:27 -0500

    Add files via upload

---
+++ Vista/html/cliente.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Página Web del Profesor</title>
+        <link href="Vista\imagenes\icono.ico" rel="icon" />
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilos.css" media="all and (min-width: 621px)"> 
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilosCelular.css" media="all and (min-width: 0px) and (max-width: 620px)">
+        <script src="Vista/js/funciones.js"></script>
+    </head>
+    <body>
+        <div id="cabecera">
+            <div id="header">
+                <ul class="nav"> 
+                    <li><a href="index.php?accion=Formulario">Agendar clase</a></li>
+                    <li><a href="index.php?accion=Cliente">Historial de reservas</a></li>
+                    <li><a href="index.php?accion=Inicio">Cerrar sesión</a></li>
+                </ul>
+            </div>
+            <img id="imgIn" src="Vista/imagenes/login2.png">
+        </div>
+        <?php
+        session_start();
+        $alumno = $_SESSION['nombre_usuario'];
+        echo "<h1>Bienvenido $alumno</h1>";
+        ?> 
+    </body>
+</html>
---
+++ Vista/html/plantilla.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Página Web del Profesor</title>
+        <link href="Vista\imagenes\icono.ico" rel="icon" />
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilos.css" media="all and (min-width: 621px)"> 
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilosCelular.css" media="all and (min-width: 0px) and (max-width: 620px)">
+    </head>
+    <body> 
+        <div id="cabecera">
+            <div id="header">
+                <ul class="nav">
+                    <li><a href="index.php?accion=Inicio">Inicio</a></li>
+                    <li><a href="index.php?accion=Formulario">Agendar clase</a></li>
+                    <li><a href="">Servicios</a>
+                        <ul>
+                            <li><a href="index.php?accion=Matematicas">Matemáticas</a></li>
+                            <li><a href="index.php?accion=Fisica">Física</a></li>
+                            <li><a href="index.php?accion=Quimica">Química</a></li>
+                            <li><a href="index.php?accion=Programacion">Programación</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php?accion=Sesion">Iniciar sesión</a>
+                        <ul>
+                            <li><a href="index.php?accion=Registro">Registrarse</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="agrupar">
+            <div id="contenido">
+                <h2>Título de página</h2>
+                <p>Contenido de la página</p>
+            </div>
+        </div>
+    </body>
+</html>
