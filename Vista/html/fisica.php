d6529d3f8bed9612c150e06d9384b1f7c4b61285
AlexisAldahirGonzalezOjeda <alexisgonzalez120021@gmail.com>
2022-08-03 23:10:56 -0500

    Add files via upload

---
+++ Vista/html/fisica.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Página Web del Profesor</title>
+        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
+        <link href="Vista\imagenes\icono.ico" rel="icon" />
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilos.css" media="all and (min-width: 621px)"> 
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilosCelular.css" media="all and (min-width: 0px) and (max-width: 620px)">
+    </head>
+    <body> 
+        <div id="cabecera">
+            <div id="header">
+                <ul class="nav">
+                    <li><a href="index.php?accion=Inicio">Inicio</a></li> 
+                    <li><a href="index.php?accion=Matematicas">Servicios</a>
+                        <ul>
+                            <li><a href="index.php?accion=Matematicas">Matemáticas</a></li>
+                            <li><a href="index.php?accion=Fisica">Física</a></li>
+                            <li><a href="index.php?accion=Quimica">Química</a></li>
+                            <li><a href="index.php?accion=Programacion">Programación</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php?accion=Sesion">Iniciar sesión</a></li> 
+                    <li><a href="index.php?accion=Registro">Crear cuenta</a></li> 
+                </ul>
+            </div>
+        </div> 
+        <div class="servicio"> 
+            <h1>Física</h1>
+            <p>¡Hola! Entre los temas de física en los que podrás agendar clases se encuentran:</p>
+            <ul>
+                <li>Movimiento uniformemente acelerado.</li>
+                <li>Leyes de Newton.</li> 
+                <li>Dinámica angular.</li>
+                <li>Movimiento armónico simple.</li>
+                <li>Estática de Fluidos.</li>
+                <li>Dinámica de Fluidos.</li>
+                <li>Termodinámica.</li>
+                <li>Física eléctrica.</li>
+                <li>Física magnética.</li>
+                <li>Física moderna.</li>
+            </ul> 
+            <br>
+            <p id="next"><a href="index.php?accion=Quimica">Siguiente</a></p>
+            <p id="back"><a href="index.php?accion=Matematicas">Anterior</a></p>  
+        </div>
+    </body>
+</html>
